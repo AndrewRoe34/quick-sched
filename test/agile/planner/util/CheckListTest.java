e00b866172c8ecccecd45e338cc8601e2b660764
LuciaLanganey <langaneylucia03@gmail.com>
2023-05-06 21:26:36 -0700

    [Implementation] added ChecklistTest and added author tag in CardTest

---
+++ test/agile/planner/util/CheckListTest.java
@@ -0,0 +1,72 @@
+package agile.planner.util;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author lucialanganey
+ */
+public class CheckListTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void setName() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getChecklistId() {
+    }
+
+    @Test
+    public void addItem() {
+    }
+
+    @Test
+    public void removeItem() {
+    }
+
+    @Test
+    public void shiftItem() {
+    }
+
+    @Test
+    public void getItem() {
+    }
+
+    @Test
+    public void markItem() {
+    }
+
+    @Test
+    public void resetCheckList() {
+    }
+
+    @Test
+    public void size() {
+    }
+
+    @Test
+    public void getPercentage() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+
+    @Test
+    public void editItem() {
+    }
+
+    @Test
+    public void getId() {
+    }
+}
