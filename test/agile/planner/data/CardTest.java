7526608e049b7d7c58ea5fe3cd3883facb82c140
AndrewRoe34 <andrewroe34@yahoo.com>
2023-05-06 23:18:51 -0400

    [Test] Set up CardTest skeleton

---
+++ test/agile/planner/data/CardTest.java
@@ -0,0 +1,43 @@
+package agile.planner.data;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Tests Card functionality
+ *
+ * @author
+ */
+public class CardTest {
+
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void getLabel() {
+    }
+
+    @Test
+    public void setLabel() {
+    }
+
+    @Test
+    public void getList() {
+    }
+
+    @Test
+    public void setList() {
+    }
+
+    @Test
+    public void addTask() {
+    }
+
+    @Test
+    public void removeTask() {
+    }
+}
