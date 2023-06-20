e6dd5307bb79b076f576c21a3613bd74a7599b22
AndrewRoe34 <andrewroe34@yahoo.com>
2023-06-20 13:19:32 -0400

    [Skeleton] Added skeleton and notes for FunctionState

--- src/agile/planner/scripter/FunctionState.java
+++ src/agile/planner/scripter/FunctionState.java
@@ -4,9 +4,11 @@ import agile.planner.data.Board;
 import agile.planner.data.Card;
 import agile.planner.data.Label;
 import agile.planner.data.Task;
+import agile.planner.scripter.exception.UnknownClassException;
 import agile.planner.util.CheckList;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class FunctionState extends State {
 
@@ -40,12 +42,15 @@ public class FunctionState extends State {
          * 3. Store the values inside the associated global function variables
          * 4. Call each function with the correct variables (will need boolean variable to make this easier)
          */
+        Scanner  strScanner = new Scanner(line);
+        String type = strScanner.next();
 
         //process arguments (and ensure they match)
 
         //update variables depending on what's provided
 
         //process each line of code while making function calls here
+        parseFunction(type, line);
 
         //reset all the variables
         resetVariables();
@@ -82,5 +87,35 @@ public class FunctionState extends State {
         boards = null;
         labels = null;
         inFunction = false;
+    }
+
+    private void parseFunction(String type, String line) {
+        if("task:".equals(type)) {
+            new TaskState().processFunc(line);
+        } else if("print:".equals(type)) {
+            new PrintState().processFunc(line);
+        } else if("label:".equals(type)) {
+            new LabelState().processFunc(line);
+        } else if("day:".equals(type)) {
+            new DayState().processFunc(line);
+        } else if(type.charAt(0) == '#') {
+            //do nothing
+        } else if("checklist:".equals(type)) {
+            new CheckListState().processFunc(line);
+        } else if("card:".equals(type)) {
+            new CardState().processFunc(line);
+        } else if("board:".equals(type)) {
+            new BoardState().processFunc(line);
+        } else if("add:".equals(type)) {
+            new AddState().processFunc(line);
+        } else if("edit:".equals(type)) {
+            new EditState().processFunc(line);
+        } else if("remove:".equals(type)) {
+            new RemoveState().processFunc(line);
+        } else if(funcMap.containsKey(type)) {
+            new FunctionState().processFunc(line);
+        } else {
+            throw new UnknownClassException();
+        }
     }
 }
