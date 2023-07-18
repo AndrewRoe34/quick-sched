c1ed02ee9e1e2d725266a1b64982cb0ce747cb19
AndrewRoe34 <andrewroe34@yahoo.com>
2023-07-17 23:17:17 -0400

    [Implement] Draft UI for script interface

--- src/agile/planner/scripter/Test.java
+++ src/agile/planner/scripter/Test.java
@@ -3,12 +3,35 @@ package agile.planner.scripter;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Test {
 
     public static void main(String[] args) throws IOException {
         ScriptContext sc = new ScriptContext();
-        String script = new String(Files.readAllBytes(Paths.get("data/test2.smpl")));
-        sc.executeScript(script);
+        File file = new File("data/");
+        File[] list = file.listFiles();
+        List<File> files = new ArrayList<>();
+        assert list != null;
+        System.out.println("Files:");
+        int i = 0;
+        for(File l : list) {
+            if (l.getName().length() > 5 && ".smpl".equals(l.getName().substring(l.getName().length() - 5))) {
+                files.add(l);
+                System.out.println((files.size() - 1) + ". " + l.getName());
+            }
+            i++;
+        }
+        System.out.print("\nChoose from the above: ");
+        Scanner input = new Scanner(System.in);
+        if(input.hasNextInt()) {
+            int idx = input.nextInt();
+            if(idx >= 0 && idx < files.size()) {
+                String script = new String(Files.readAllBytes(Paths.get(files.get(idx).getAbsolutePath())));
+                sc.executeScript(script);
+            }
+        }
     }
 }
