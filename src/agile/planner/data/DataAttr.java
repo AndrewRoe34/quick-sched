fe95a4db683b195d4b83e6ab14695e7c14508671
AndrewRoe34 <andrewroe34@yahoo.com>
2023-07-30 08:26:45 -0400

    [Implement] Added enum for data attributes

---
+++ src/agile/planner/data/DataAttr.java
@@ -0,0 +1,17 @@
+package agile.planner.data;
+
+//allows for working with attributes and possible custom functions
+public enum DataAttr {
+    ID,
+    TITLE,
+    COLOR,
+    DUE_DATE,
+    TOTAL_HOURS,
+    SUB_TOTAL_HOURS,
+    AVG_HOURS,
+    TASKS,
+    CHECKLIST,
+    LABELS,
+    ADD,
+    REMOVE
+}
