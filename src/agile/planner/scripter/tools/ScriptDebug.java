92510b63c39e655f2f44d3cdaf65fe313da24df0
AndrewRoe34 <andrewroe34@yahoo.com>
2023-09-12 21:38:02 -0400

    [Note] Note for ScriptDebug

--- src/agile/planner/scripter/tools/ScriptDebug.java
+++ src/agile/planner/scripter/tools/ScriptDebug.java
@@ -1,38 +1,13 @@
 package agile.planner.scripter.tools;
 
 public class ScriptDebug {
-
-    public static String add() {
-        return null;
-    }
-
     /*
-    Reproduce statements but with debug
+    Will need to manage line number as well as a variety of commands the user may enter here:
+    n/next
+    c/continue
+    b/break #
+    s/step into
 
-    Example:
-    setup_task: task, label, card
-        #this should be fine
-        add: _label, _task
-        add: _task, _card
-
-    #data
-    task: HW, 5, 0
-    label: MA, green
-    card: MATH
-
-    #custom function call
-    setup_task: _task, _label, _card
-
-    OUTPUT:
-    task: HW, 5, 0      [T0]
-    label: MA, green    [L0]
-    card: MATH          [C1]
-    setup_task: _task, _label, _card    [T0], [L0], [C0]
-      add: _label, _task  [L0] -> [T0]
-      add: _task, _card   [T0] -> [C0]
-     */
-
-    /*
-    One option is to write it so that it works similar to GCC (shitty, but impressive)
+    Goal is to emulate GCC and its debugging functionality
      */
 }
