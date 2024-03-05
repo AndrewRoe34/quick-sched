95da3d1446b236cebc38162ee7f35bdda0c4f197
AndrewRoe34 <andrewroe34@yahoo.com>
2024-03-04 19:20:00 -0500

    [Implement] Skeleton for JsonHandler is complete

--- src/main/java/agile/planner/util/JsonHandler.java
+++ src/main/java/agile/planner/util/JsonHandler.java
@@ -6,9 +6,16 @@ import java.util.List;
 
 public class JsonHandler {
 
-    public static void processJsonCommand(String jsonStr) {
+    public static void readJsonFile(String jsonStr) {
         Gson gson = new Gson();
         Board board = gson.fromJson(jsonStr, Board.class);
+        System.out.println("Board\nid=" + board.getId() + ", name=" + board.getBoardName() + ", cards\n");
+        for(Card c1 : board.getCardList()) {
+
+        }
+    }
+
+    public static void writeJsonFile() {
 
     }
 
@@ -27,6 +34,18 @@ public class JsonHandler {
 
         public List<Card> getCardList() {
             return cardList;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public void setBoardName(String boardName) {
+            this.boardName = boardName;
+        }
+
+        public void setCardList(List<Card> cardList) {
+            this.cardList = cardList;
         }
     }
 
@@ -51,6 +70,22 @@ public class JsonHandler {
         public List<Task> getTaskList() {
             return taskList;
         }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public void setCardName(String cardName) {
+            this.cardName = cardName;
+        }
+
+        public void setLabelList(List<Label> labelList) {
+            this.labelList = labelList;
+        }
+
+        public void setTaskList(List<Task> taskList) {
+            this.taskList = taskList;
+        }
     }
 
     private static class Label {
@@ -63,6 +98,14 @@ public class JsonHandler {
 
         public int getColor() {
             return color;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public void setColor(int color) {
+            this.color = color;
         }
     }
 
@@ -97,6 +140,30 @@ public class JsonHandler {
         public Checklist getChecklist() {
             return checklist;
         }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setHours(int hours) {
+            this.hours = hours;
+        }
+
+        public void setDueDate(String dueDate) {
+            this.dueDate = dueDate;
+        }
+
+        public void setLabelList(List<Label> labelList) {
+            this.labelList = labelList;
+        }
+
+        public void setChecklist(Checklist checklist) {
+            this.checklist = checklist;
+        }
     }
 
     private static class Checklist {
@@ -115,6 +182,18 @@ public class JsonHandler {
         public int getPercentComplete() {
             return percentComplete;
         }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public void setChecklistItemList(List<ChecklistItem> checklistItemList) {
+            this.checklistItemList = checklistItemList;
+        }
+
+        public void setPercentComplete(int percentComplete) {
+            this.percentComplete = percentComplete;
+        }
     }
 
     private static class ChecklistItem {
@@ -132,6 +211,18 @@ public class JsonHandler {
 
         public boolean isComplete() {
             return complete;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public void setDescr(String descr) {
+            this.descr = descr;
+        }
+
+        public void setComplete(boolean complete) {
+            this.complete = complete;
         }
     }
 }
